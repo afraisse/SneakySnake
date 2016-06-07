3f535dd07b56153d78470ace68cc8d002d9192ab
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2016-06-07 16:24:08 +0200

    Move snake in straight line

--- src/main/java/microej/labs/eval/GameProgram.java
+++ src/main/java/microej/labs/eval/GameProgram.java
@@ -3,6 +3,8 @@ package microej.labs.eval;
 import java.util.ArrayList;
 import java.util.List;
 
+import ej.bon.Timer;
+import ej.bon.TimerTask;
 import ej.microui.display.Colors;
 import ej.microui.display.Display;
 import ej.microui.display.Displayable;
@@ -11,8 +13,11 @@ import ej.microui.util.EventHandler;
 
 public class GameProgram extends Displayable implements EventHandler {
 	
-	int height;
-	int width;
+	int screenHgt;
+	int screenWdt;
+	
+	int canvasWdt;
+	int canvasHgt;
 	
 	int length = 4;
 	int rectSize = 4;
@@ -21,21 +26,35 @@ public class GameProgram extends Displayable implements EventHandler {
 	public GameProgram() {
 		super(Display.getDefaultDisplay());
 		Display disp = this.getDisplay();
-		height = disp.getHeight();
-		width = disp.getWidth();
+		screenHgt = disp.getHeight();
+		screenWdt = disp.getWidth();
+		canvasWdt = screenWdt / rectSize;
+		canvasHgt = screenHgt / rectSize;
+		
 		snake = new ArrayList<Rect>();
 		
 		// First rect
-		Rect rect = new Rect(width/2, height/2);
+		Rect rect = new Rect(screenWdt/2, screenHgt/2);
 		snake.add(rect);
 		for (int i = 1; i< length+1; i++) {
-			snake.add(new Rect(width/2 -i*rectSize, height/2));
+			snake.add(new Rect(screenWdt/2 -i*rectSize, screenHgt/2));
 		}
+		
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				Rect head = snake.get(0);
+				snake.add(0, new Rect(head.x+rectSize, head.y));
+				snake.remove(snake.size()-1);
+				repaint();
+			}
+		}, 0, 200);
 	}
 
 	@Override
 	public boolean handleEvent(int event) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -43,7 +62,7 @@ public class GameProgram extends Displayable implements EventHandler {
 	public void paint(GraphicsContext g) {
 		// White background
 		g.setColor(Colors.WHITE);
-		g.fillRect(0, 0, width, height);	
+		g.fillRect(0, 0, screenWdt, screenHgt);	
 	
 		//Draw snake
 		g.setColor(Colors.BLACK);
