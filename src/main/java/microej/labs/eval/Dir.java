8186096e7256c95920afddbf4e7f7d0b9d5f2adf
theophileMorin <theophile.morin.pro@gmail.com>
2016-06-07 16:08:10 +0200

    Ajout de la gestion des evenements, ajout de la notion de direction

---
+++ src/main/java/microej/labs/eval/Dir.java
@@ -0,0 +1,10 @@
+package microej.labs.eval;
+
+public class Dir {
+
+	final static int DROITE = 1;
+	final static int BAS = 2;
+	final static int GAUCHE = 4;
+	final static int HAUT = 8;
+
+}
--- src/main/java/microej/labs/eval/GameProgram.java
+++ src/main/java/microej/labs/eval/GameProgram.java
@@ -7,13 +7,18 @@ import ej.microui.display.Colors;
 import ej.microui.display.Display;
 import ej.microui.display.Displayable;
 import ej.microui.display.GraphicsContext;
+import ej.microui.event.Event;
+import ej.microui.event.generator.Pointer;
 import ej.microui.util.EventHandler;
 
 public class GameProgram extends Displayable implements EventHandler {
-	
+
+
 	int height;
 	int width;
-	
+
+	int currentDir;
+
 	int length = 4;
 	int rectSize = 4;
 	List<Rect> snake;
@@ -24,7 +29,7 @@ public class GameProgram extends Displayable implements EventHandler {
 		height = disp.getHeight();
 		width = disp.getWidth();
 		snake = new ArrayList<Rect>();
-		
+
 		// First rect
 		Rect rect = new Rect(width/2, height/2);
 		snake.add(rect);
@@ -33,29 +38,75 @@ public class GameProgram extends Displayable implements EventHandler {
 		}
 	}
 
-	@Override
-	public boolean handleEvent(int event) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
+	//----------------------------------------------
+	//--------- Gestion du rafraichissement
+	//----------------------------------------------
 	@Override
 	public void paint(GraphicsContext g) {
 		// White background
 		g.setColor(Colors.WHITE);
 		g.fillRect(0, 0, width, height);	
-	
+
 		//Draw snake
 		g.setColor(Colors.BLACK);
 		for (Rect rect : snake) {
 			g.drawRect(rect.x, rect.y, rectSize, rectSize);
 		}
-		
+
 	}
 
+
+
+	//----------------------------------------------
+	//--------- Gestion des evenements
+	//----------------------------------------------
 	@Override
 	public EventHandler getController() {
 		return this;
+	}
+
+	@Override
+	public boolean handleEvent(int event) {
+		if(Event.getType(event) == Event.POINTER)
+		{
+			if(Pointer.isPressed(event) || Pointer.isDragged(event))
+			{
+				Pointer ptr= (Pointer)Event.getGenerator(event);
+				if(ptr.getX() >= (height/2)) 
+					currentDir = Dir.DROITE;
+				else
+					currentDir = Dir.GAUCHE;
+				return true;
+			}
+		}	
+		return false;
+	}
+
+	/**
+	 * Change la direction courante en tournant à droite (true) ou a gauche (true)
+	 * @param droite
+	 */
+	private void changeDirection(boolean droite) {
+		switch(currentDir) {
+		case Dir.DROITE: 
+			if(droite) currentDir = Dir.BAS;
+			else currentDir = Dir.HAUT;
+			break;
+		case Dir.BAS: 
+			if(droite) currentDir = Dir.GAUCHE;
+			else currentDir = Dir.DROITE;
+			break;
+		case Dir.GAUCHE: 
+			if(droite) currentDir = Dir.HAUT;
+			else currentDir = Dir.BAS;
+			break;
+		case Dir.HAUT: 
+			if(droite) currentDir = Dir.DROITE;
+			else currentDir = Dir.GAUCHE;
+			break;
+		default : break;
+		}
 	}
 
 }
